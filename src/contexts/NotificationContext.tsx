931565e8d85c94197c4eab82ea90976e1c5e3984
Hector Diaz <hdiaz@nectia.com>
2025-03-11 18:08:29 -0300

    feat(clase-13): add NotificationContext (Provider Pattern)

---
+++ src/contexts/NotificationContext.tsx
@@ -0,0 +1,28 @@
+import { createContext, useState } from 'react';
+
+interface NotificationContextType {
+  message: string | null;
+  hideNotification: () => void;
+  showNotification: (message: string) => void;
+}
+
+export const NotificationContext = createContext<
+  NotificationContextType | undefined
+>(undefined);
+
+const NotificationProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [message, setMessage] = useState<string | null>(null);
+
+  const hideNotification = () => setMessage(null);
+  const showNotification = (message: string) => {
+    setMessage(message);
+    setTimeout(hideNotification, 5000);
+  };
+
+  return (
+    <NotificationContext.Provider value={{ message, hideNotification, showNotification }}>
+      {children}
+    </NotificationContext.Provider>
+};
+
+export default NotificationProvider;
